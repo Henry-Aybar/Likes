8ea0c6421df061281090580524d44d8985eedb8e
Henry Aybar <aybar.henry.usmc@gmail.com>
2021-08-26 02:13:41 -0500

    inital commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function addLike0(){
+    document.querySelector("#likeNum0").innerHTML++;
+}
+
+function addLike1(){
+    document.querySelector("#likeNum1").innerHTML++;
+}
+
+function addLike2(){
+    document.querySelector("#likeNum2").innerHTML++;
+}
